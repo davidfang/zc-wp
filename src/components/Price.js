e00b96d027259a229b218f27c7727e0677ab937f
davidfang <mw_fang@163.com>
2016-06-10 14:36:46 +0800

    去除一个无用的样式变量

--- src/components/Price.js
+++ src/components/Price.js
@@ -2,57 +2,52 @@
  * Created by david_fang on 2016/6/9.
  */
 import React from 'react';
-import { red500, lightGreen500} from 'material-ui/styles/colors';
+import {red500, lightGreen500} from 'material-ui/styles/colors';
 import FontIcon from 'material-ui/FontIcon';
-import { TableRowColumn} from 'material-ui/Table';
+import {TableRowColumn} from 'material-ui/Table';
 
-class Price extends React.Component{
-  render(){
+class Price extends React.Component {
+  render() {
     let styles = {
-      up:{
-        color:'white',
-        background:red500
-      },
-      down:{
+      up: {
         color: 'white',
-        background:lightGreen500
+        background: red500
       },
-      iconUp:{
-        color:red500,
-        background:'white',
-        marginLeft:15,
-        marginRight:15
+      down: {
+        color: 'white',
+        background: lightGreen500
       },
-      iconDown:{
+      iconUp: {
+        color: red500,
+        background: 'white',
+        marginLeft: 15,
+        marginRight: 15
+      },
+      iconDown: {
         color: lightGreen500,
-        background:'white',
-        marginLeft:15,
-        marginRight:15
+        background: 'white',
+        marginLeft: 15,
+        marginRight: 15
       }
     };
     var lastStyle = {},
-    changeStyle = {},
       icon = '',
-    iconStyle ={};
+      iconStyle = {};
 
     if (this.props.stock.change < 0) {
-      lastStyle = styles.down ;
-        changeStyle = {};
+      lastStyle = styles.down;
       icon = 'trending_down';
-        iconStyle =styles.iconDown;
-    }else if (this.props.stock.change > 0){
+      iconStyle = styles.iconDown;
+    } else if (this.props.stock.change > 0) {
       lastStyle = styles.up;
-      changeStyle = {};
       icon = 'trending_up';
-      iconStyle =styles.iconUp;
+      iconStyle = styles.iconUp;
     }
 
 
-
-
     return <TableRowColumn style={lastStyle}>
-      <span>{this.props.stock.symbol}  <big>{this.props.stock.last}</big></span>
-      <FontIcon className="material-icons" style={iconStyle} >{icon}</FontIcon>
+      <span>{this.props.stock.symbol} <big>{this.props.stock.last}</big></span>
+      <FontIcon className="material-icons" style={iconStyle}>{icon}</FontIcon>
       <i>{this.props.stock.change}</i>
     </TableRowColumn>
   }
