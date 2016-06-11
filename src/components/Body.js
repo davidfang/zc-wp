f00bf5f38f436e053df5f32c57a08490c36e0dc0
davidfang <mw_fang@163.com>
2016-06-11 09:21:26 +0800

    将中间部分和头部、脚部的刷新分开

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,27 +5,52 @@ import React from 'react';
 
 import GoodsBox from './GoodsBox';
 import StockCharts from './StockCharts';
-
+import feed from './Feed';
 class Body extends React.Component {
   constructor(props) {
     super(props);
+    this.watchStock = this.watchStock.bind(this);
+    this.unwatchStock = this.unwatchStock.bind(this);
+
+    //feed.watch(['MCD', 'BA',  'LLY', 'GM', 'GE', 'UAL', 'WMT', 'AAL', 'JPM']);
+    feed.watch(['BAC', 'BA']);
+    var stocks = {};
+    feed.onChange(function (stock) {
+      stocks[stock.symbol] = stock;
+      this.setState({stocks: stocks, last: stock});
+      //console.log(stock);
+    }.bind(this));
+    this.state = {stocks: stocks};
+  }
+
+  watchStock(symbols) {
+    symbols = symbols.replace(/ /g, '');
+    var arr = symbols.split(',');
+    feed.watch(arr);
+  }
+
+  unwatchStock(symbol) {
+    feed.unwatch(symbol);
+    var stocks = this.state.stocks;
+    delete stocks[symbol];
+    this.setState({stocks: stocks});
   }
 
   render() {
     //let style = {};
     return <div>
-      <GoodsBox  stocks={this.props.stocks} last={this.props.last}/>
+      <GoodsBox  stocks={this.state.stocks} last={this.state.last}/>
       <StockCharts />
     </div>
 
   }
 }
 // porps 的默认值
-Body.defaultProps = {
+/*Body.defaultProps = {
   stocks: {
     'BAC': {symbol: 'BAC', open: 15.84,last:15.84,high:15.84,low:15.84},
     'BA': {symbol: 'BA', open: 154.50,last:154.50,high:154.50,low:154.50}
   },
   last: {symbol: 'BA', open: 154.50,last:154.50,high:154.50,low:154.50,change:0.5}
-}
+}*/
 export default Body;
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,43 +6,19 @@ import Head from './Head';
 import Body from './Body';
 import Foot from './Foot';
 
-import feed from './Feed';
+
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.watchStock = this.watchStock.bind(this);
-    this.unwatchStock = this.unwatchStock.bind(this);
-
-    //feed.watch(['MCD', 'BA',  'LLY', 'GM', 'GE', 'UAL', 'WMT', 'AAL', 'JPM']);
-    feed.watch(['BAC', 'BA']);
-    var stocks = {};
-    feed.onChange(function (stock) {
-      stocks[stock.symbol] = stock;
-      this.setState({stocks: stocks, last: stock});
-      //console.log(stock);
-    }.bind(this));
-    this.state = {stocks: stocks};
   }
 
-  watchStock(symbols) {
-    symbols = symbols.replace(/ /g, '');
-    var arr = symbols.split(',');
-    feed.watch(arr);
-  }
-
-  unwatchStock(symbol) {
-    feed.unwatch(symbol);
-    var stocks = this.state.stocks;
-    delete stocks[symbol];
-    this.setState({stocks: stocks});
-  }
 
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
         <div>
           <Head />
-          <Body stocks={this.state.stocks} last={this.state.last}/>
+          <Body />
           <Foot />
         </div>
       </MuiThemeProvider>
