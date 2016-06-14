6218a87eba204f36e88c0ea750d38f130bdf0374
davidfang <mw_fang@163.com>
2016-06-14 23:08:34 +0800

    异步加载数据可以实现，再完美一点

--- src/components/Body.js
+++ src/components/Body.js
@@ -84,19 +84,8 @@ class Body extends React.Component {
   /**
    * 设置数据
    */
-  setData(){
-    d3.tsv('./data/MSFT.tsv', function(err, data) {
-      data.forEach((d, i) => {
-        d.date = new Date(parseDate(d.date).getTime());
-        d.open = +d.open;
-        d.high = +d.high;
-        d.low = +d.low;
-        d.close = +d.close;
-        d.volume = +d.volume;
-        // console.log(d);
-      });
-      this.changeState('data',data);
-    });
+  setData(data){
+    this.changeState('data',data);
   }
   /**
    * 更新数据
@@ -129,7 +118,7 @@ class Body extends React.Component {
       <StockCharts
         stocks={this.state.stocks}
         last={this.state.last}
-        name={this.state.checkedStock}
+        checkedStock={this.state.checkedStock}
         data={this.props.data}
         timeType={this.state.timeType}
         changeState={this.changeState}
--- src/components/StockCharts.js
+++ src/components/StockCharts.js
@@ -5,19 +5,50 @@ import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import CandleStickChartWithEdge from '../lib/charts/CandleStickChartWithEdge';
 import TimeType from './TimeType';
+var d3 = require('d3');
+var parseDate = d3.time.format('%Y-%m-%d').parse ;
 class StockCharts extends React.Component{
   constructor(props) {
     super(props);
   }
   shouldComponentUpdate(nextProps, nextState){
-    console.log('shouldComponentUpdate');
+    /*console.log('shouldComponentUpdate');
     console.log('nextProps');
     console.log(nextProps);
     console.log('thisProps');
     console.log(this.props);
-    return false;
-    return nextProps.data == this.props.data;
+    return false;*/
+    if(nextProps.timeType !== this.props.timeType){//K线时间周期发生变化
+      this.setData();
+    }
+    if(nextProps.checkedStock !== this.props.checkedStock){//选择股票名称发生变化
+      this.setData();
+    }
+    return nextProps.data !== this.props.data;
   }
+  setData(){
+    var setData = this.props.setData;
+    //* 设置数据
+    d3.tsv('./data/MSFT.tsv', function(err, data) {
+      data.forEach((d, i) => {
+        d.date = new Date(parseDate(d.date).getTime());
+        d.open = +d.open;
+        d.high = +d.high;
+        d.low = +d.low;
+        d.close = +d.close;
+        d.volume = +d.volume;
+        // console.log(d);
+      });
+      console.log('异步加载data');
+      console.log(data);
+      console.log('异步加载data');
+      setData(data);
+    });
+  }
+  componentDidMount(){
+    this.setData();
+  }
+
 
   render(){
     console.log('StockCharts执行一次');
@@ -26,7 +57,7 @@ class StockCharts extends React.Component{
     });
 
     return <div>
-      <div>{this.props.name}</div>
+      <div>{this.props.checkedStock}</div>
       <div>{timeTypes}</div>
       <CandleStickChartWithEdge data={this.props.data} type="hybrid"/>
     </div>
