22bb9a5e1b92d71b821d4275a6c8ed13643b451f
davidfang <mw_fang@163.com>
2016-08-04 16:16:11 +0800

    修改client.js，增加清理数据操作

--- service/client.js
+++ service/client.js
@@ -5,6 +5,11 @@ db.redis(function (err, client) {
     console.error(err.toString());
     return process.exit(1);
   }
+  /*
+  //增加清理数据操作
+  config.stocks.map(function(stock){
+    cleanData (client,stock.symbol + ':last',stock.symbol + ':realTime');
+  });*/
 
   //console.log('open , high , low , close  ,change ,volume ,date');
   setInterval(function(){
@@ -18,6 +23,21 @@ db.redis(function (err, client) {
 
 
 });
+
+/**
+ * 清除数据
+ * @param client   Redis客户端实例化对象
+ * @param lastHash  当前数据存放的hash
+ * @param realTimeHash  分时数据存放的hash
+ */
+function cleanData (client,lastHash,realTimeHash) {
+  client.del(lastHash);
+  client.del(realTimeHash+':now',realTimeHash+':now:zset');
+  client.del(realTimeHash+':M1',realTimeHash+':M1:zset');
+  client.del(realTimeHash+':M5',realTimeHash+':M5:zset');
+  client.del(realTimeHash+':M15',realTimeHash+':M15:zset');
+  client.del(realTimeHash+':H1',realTimeHash+':H1:zset');
+}
 /**
  * 生成数据
  * @param client   Redis客户端实例化对象
